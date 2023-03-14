47943b797e545055703c33cb199d5eabd28d7bf5
Shadvonic <moxeymarcus28@gmail.com>
2023-03-13 22:31:23 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Marc Moxey on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
